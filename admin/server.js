9791a03253bffd2f9ad60fd15971cf8c23c0b01c
Kallol Pratim <ksaikia615@gmail.com>
2016-08-29 17:05:17 +0530

    admin view

---
+++ admin/server.js
@@ -0,0 +1,102 @@
+var express = require('express');
+var request = require('superagent');
+
+var app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/public/views/');
+
+app.use(express.static(__dirname + '/public'));
+
+var NON_INTERACTIVE_CLIENT_ID = 'AACNWJji5eACjN4DsmvVrilsz4Z077yk';
+var NON_INTERACTIVE_CLIENT_SECRET = 'dKbE55LRKXNPUipffot0O7t96fBSxvPMW5uCDxdUIqvZUJPZL2rFaed8a5CwK7U8';
+
+var authData = {
+    client_id: NON_INTERACTIVE_CLIENT_ID,
+    client_secret: NON_INTERACTIVE_CLIENT_SECRET,
+    grant_type: 'client_credentials',
+    audience: 'http://movieanalyst.com'
+}
+
+// First, authenticate this client and get an access_token
+// This could be cached
+function getAccessToken(req, res, next) {
+    request
+        .post('https://kallolpratim.auth0.com/oauth/token')
+        .send(authData)
+        .end(function(err, res) {
+            req.access_token = res.body.access_token
+            next();
+        })
+}
+
+app.get('/', function(req, res) {
+    res.render('index');
+})
+
+app.get('/movies', getAccessToken, function(req, res) {
+    request
+        .get('http://localhost:8080/movies')
+        .set('Authorization', 'Bearer ' + req.access_token)
+        .end(function(err, data) {
+            console.log(data);
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                var movies = data.body;
+                res.render('movies', {
+                    movies: movies
+                });
+            }
+        })
+})
+
+app.get('/authors', getAccessToken, function(req, res) {
+    request
+        .get('http://localhost:8080/reviewers')
+        .set('Authorization', 'Bearer ' + req.access_token)
+        .end(function(err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                var authors = data.body;
+                res.render('authors', {
+                    authors: authors
+                });
+            }
+        })
+})
+
+app.get('/publications', getAccessToken, function(req, res) {
+    request
+        .get('http://localhost:8080/publications')
+        .set('Authorization', 'Bearer ' + req.access_token)
+        .end(function(err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                var publications = data.body;
+                res.render('publications', {
+                    publications: publications
+                });
+            }
+        })
+})
+
+app.get('/pending', getAccessToken, function(req, res) {
+    request
+        .get('http://localhost:8080/pending')
+        .set('Authorization', 'Bearer ' + req.access_token)
+        .end(function(err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                var movies = data.body;
+                res.render('pending', {
+                    movies: movies
+                });
+            }
+        })
+})
+
+app.listen(4000);
